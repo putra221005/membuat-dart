977c4723d94478fd709249df0233c94be17df887
putra221005 <yusufmaulanaputramuhammad@gmail.com>
2022-12-06 20:40:42 +0700

    membuat dart

---
+++ bin/dart_les.dart
@@ -0,0 +1,5 @@
+import 'package:dart_les/dart_les.dart' as dart_les;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_les.calculate()}!');
+}
---
+++ test/dart_les_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_les/dart_les.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
